0612aaa447422ac9e15d231c27a2b768ba426092
LevMarkos <levmarcosyan@mail.ru>
2025-03-04 18:12:40 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,6 @@ public class Main {
             multiplesOfFive.append("c=").append(c).append(", ");
             foundMultipleOfFive = true;
         }
-
         if (foundMultipleOfFive) {
             multiplesOfFive.setLength(multiplesOfFive.length() - 2);
             System.out.println(multiplesOfFive);
@@ -30,21 +29,13 @@ public class Main {
             System.out.println("нет значений, кратных 5");
         }
         System.out.println("Результат целочисленного деление a на b: " + (a / b));
-
         System.out.println("Результат деления a на b: " + ((double) a / b));
-
         System.out.println("Результат деления a на b с округлением в большую сторону: " + (int) Math.ceil((double) a / b));
-
         System.out.println("Результат деления a на b с округлением в меньшую сторону: " + (int) Math.floor((double) a / b));
-
         System.out.println("Результат деления a на b с математическим округлением: " + Math.round((double) a / b));
-
         System.out.println("Остаток от деления b на c: " + (b % c));
-
         System.out.println("Наименьшее значение из a и b: " + Math.min(a, b));
-
         System.out.println("Наибольшее значение из b и c: " + Math.max(b, c));
-
         scanner.close();
     }
 }
